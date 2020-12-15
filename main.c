ba7446f2472dc786d031152e40a762385fc5b039
EnriqueJC <75394662+EnriqueJC@users.noreply.github.com>
2020-12-15 18:00:16 -0500

    V1

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int res,cont;
+	printf("SDSD\n");
+	printf("2.\n");
+	printf("1.\n");
+	scanf("%d",&res);
+	if(res==1){
+		printf("X");
+		cont++;
+	}else{
+		printf("ERROR");
+	}
+	printf("SDSD\n");
+	printf("2.\n");
+	printf("1.\n");
+	scanf("%d",&res);
+	if(res==1){
+		printf("X");
+		cont++;
+	}else{
+		printf("ERROR");
+	}
+		printf("SDSD\n");
+	printf("2.\n");
+	printf("1.\n");
+	scanf("%d",&res);
+	if(res==1){
+		printf("X");
+		cont++;
+	}else{
+		printf("ERROR");
+	}
+	printf("%d",cont);
+	return 0;
+}
