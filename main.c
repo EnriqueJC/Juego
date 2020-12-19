54e593396f9179135426ee44bdfc9d30b33d6491
1754s <75954531+1754s@users.noreply.github.com>
2020-12-19 18:36:40 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -94,6 +94,97 @@ int main(int argc, char *argv[]) {
 		b=b+0;
 		
 }
+	system("color 6");
+	printf("¿Durante la presidencia de Sixto Duran Ballén (1992-1996) el Ecuador se enfrentó bélicamente con el Perú  en las zonas denominadas? \n");
+	system("color 1");
+	printf("1.Paquisha, Mayaycu y  Machinaza\n");
+	printf("2.Altos del Cenepa, Cueva de los Tallos, Cóndor Mirador\n");
+	printf("3.Macara, Loja, Zamora\n");
+	
+	scanf("%d",&res);
+	if(res==2){
+			system("color 2");
+		printf("Correcto");
+		b=1;
+	}else{
+		system("color");
+		printf("Incorrecto");
+		b=0;
+		
+}
+	system("color 6");
+	printf(" ¿Cual fue el presidente de Ecuador que murió en cumplimiento de sus funciones el 24 de mayo de 1981?\n");
+	system("color 1");
+	printf("1.Jaime Roldos Aguilera\n");
+	printf("2.Velasco Ibarra\n");
+	printf("3.Rodrigo Borja\n");
+	
+	scanf("%d",&res);
+	if(res==1){
+			system("color 2");
+		printf("Correcto");
+		b=b+1;
+	}else{
+		system("color 2");
+		printf("Incorrecto");
+		b=b+0;
+		
+}
+	system("color 6");
+	printf(" ¿Entre los logros más importantes del pueblo Ecuatoriano, alcanzados en la revolución liberal de 1995 se pueden mencionar?\n");
+	system("color 1");
+	printf("1.La adopción de los símbolos patrios actuales\n");
+	printf("2.construcción del ferrocarril\n");
+	printf("3.construcción del puente de la unidad nacional\n");
+	
+	scanf("%d",&res);
+	if(res==2){
+			system("color 2");
+		printf("Correcto");
+		b=b+1;
+	}else{
+		system("color 2");
+		printf("Incorrecto");
+		b=b+0;
+		
+}
+	system("color 6");
+	printf("¿La constitución que se encuentra en la vigencia fue aprobada por la última  Asamblea  Nacional que funcionaba en la denominada?\n");
+	system("color 1");
+	printf("1.Ciudad Futura\n");
+	printf("2.Ciudad Alfaro\n");
+	printf("3.Ciudad Alegría\n");
+	
+	scanf("%d",&res);
+	if(res==2){
+			system("color 2");
+		printf("Correcto");
+		b=b+1;
+	}else{
+		system("color 2");
+		printf("Incorrecto");
+		b=b+0;
+		
+}
+	system("color 6");
+	printf(" ¿El Ecuador dolarizo su economía un ? \n");
+	system("color 1");
+	printf("1.9 de enero 2000\n");
+	printf("2.9 de enero 2010\n");
+	printf("3.9 de enero 1999\n");
+	
+	scanf("%d",&res);
+	if(res==1){
+			system("color 2");
+		printf("Correcto ");
+		b=b+1;
+	}else{
+		system("color 2");
+		printf("Incorrecto");
+		b=b+0;
+		
+}
+
 
 
 return 0;
